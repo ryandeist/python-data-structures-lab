30415497a3dc53dbe0d58b402038bfe489d3f119
Ryan Deist <ryandeist@gmail.com>
2025-02-17 15:17:26 -0500

    Exercise 5 complete

--- exercises.py
+++ exercises.py
@@ -58,16 +58,30 @@ print('Exercise 3:', slice_foods())
 #
 # Create a dictionary named home_town containing the keys of city, state, and population.
 # Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
+home_town = {
+    "city": "King's Park",
+    "state": "New York",
+    "population": 16730,
+}
 
 def hometown_info():
-    home_town = {
-        "city": "King's Park",
-        "state": "New York",
-        "population": 16730,
-    }
-    
     home_town_message = f'I was born in {home_town["city"]}, {home_town["state"]} - population of {home_town["population"]:,}.'
     
     return home_town_message
 
 print('Exercise 4:', hometown_info())
+
+# Exercise 5: Iterating Over Dictionary Items
+#
+# Define an empty list named home_town_items.
+# Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
+
+def list_home_town_items():
+    home_town_items = []
+    
+    for key, val in home_town.items():
+        home_town_items.append(f'{key} = {val}')
+    
+    return home_town_items
+    
+print('Exercise 5:', list_home_town_items())
