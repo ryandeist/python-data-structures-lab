6647a0cc366a2dffcbafb5e9e3159e9e5a2bd8d2
Ryan Deist <ryandeist@gmail.com>
2025-02-17 14:32:38 -0500

    Exercise 3 completed

--- exercises.py
+++ exercises.py
@@ -35,11 +35,21 @@ print('Exercise 1:', manage_students())
 # Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
 # Create a variable named meal and assign an empty string to it.
 # Use a for loop to iterate over the strings in foods and append each string to meal.
+foods=('cupcakes', 'hamburgers', 'broccoli', 'chicken wings')
 
 def combine_foods():
-    foods=('cupcakes', 'hamburgers', 'broccoli', 'chicken wings')
     meal=''
     for food in foods:
         meal += f'{food} '
     return meal
 print('Exercise 2:', combine_foods())
+
+# Exercise 3: Slicing Tuples
+#
+# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods():
+    last_two_foods=foods[-2:]
+    return last_two_foods
+
+print('Exercise 3:', slice_foods())
